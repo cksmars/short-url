c25001a0694f90d5c4d4634ea9b56aaa1209d4c1
cksmars <cks.mars@gmail.com>
2022-05-14 21:36:27 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
 const routes = require('./routes')
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
